f42260dddde659b198244ca7e004fc3a7ef8626c
Mavovo-ii <96288027+Mavovo-ii@users.noreply.github.com>
2022-07-16 18:13:42 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,38 @@
+// change navbar style on scroll
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
+});
+
+//show FAQ answer
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+    });
+});
+
+//Show Nav Menu or Hide....Toggle in fact!!
+
+const menu = document.querySelector('.nav__menu');
+const menuBtn = document.querySelector('#open-menu-btn');
+const closeBtn = document.querySelector('#close-menu-btn');
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+//Close Nav Menu
+
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click', closeNav);
